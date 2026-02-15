12736f1fa4f9f880ca63dce38dc70e049191fd6e
seoeongeueun <erin9901@gmail.com>
2026-02-16 03:03:18 +0900

    feat: update Card component to use post data

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,18 @@
+import { WPPost } from "@/types";
 import { Tag } from "@/ui";
 import Image from "next/image";
 
-export default function Card() {
-  //TODO: API 연동 후 랜덤 썸네일 제거
-  const thumbnailSuffix = Math.random() > 0.5 ? "2" : "";
+interface CardProps {
+  post: WPPost;
+}
 
+//TODO: 카테고리 ID를 실제 카테고리 이름과 매핑하는 로직 필요
+export default function Card({ post }: CardProps) {
   return (
-    <article className="w-full h-fit flex flex-col items-start justify-start gap-spacing-3 relative after:absolute after:inset-0 after:bg-black/50 after:z-30 after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-200 after:pointer-events-none cursor-pointer">
+    <article className="grow h-auto flex flex-col items-start justify-end gap-spacing-3 relative after:absolute after:inset-0 after:bg-black/50 after:z-30 after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-200 after:pointer-events-none cursor-pointer">
       <figure className="w-full">
         <Image
-          src={`/assets/sample_thumbnail${thumbnailSuffix}.png`}
+          src={post.jetpack_featured_media_url ?? ""}
           alt="프로젝트 썸네일"
           className="w-full h-auto object-contain"
           width={230}
@@ -17,7 +20,7 @@ export default function Card() {
         />
       </figure>
       <figcaption className="w-full h-fit wrap-break-word space-y-spacing-3 font-pretendard">
-        <h3 className="text-s">프로젝트 제목 프로젝트 제목 프로젝트 제목</h3>
+        <h3 className="text-s">{post.title.rendered}</h3>
         <ul className="flex flex-wrap gap-spacing-3">
           <Tag color="#FF5733" label="Graphic" />
           <Tag color="#33C1FF" label="Space" />
--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,13 +1,18 @@
 import Card from "./Card";
-import { SearchBar } from "../ui";
+import { SearchBar } from "@/ui";
+import { getPosts } from "@/actions/posts";
+import { WPPost } from "@/types";
 
-export default function CardList() {
+export default async function CardList() {
+  const posts = await getPosts(10, 1);
+  console.log(posts);
+
   return (
     <main className="relative top-30 px-margin flex flex-col">
       <SearchBar />
-      <section className="grid grid-cols-4 gap-spacing-10 py-[5rem]">
-        {Array.from({ length: 7 }, (_, i) => (
-          <Card key={i} />
+      <section className="flex flex-wrap gap-x-spacing-10 space-y-spacing-40 py-[5rem] w-full">
+        {posts.map((post: WPPost, i) => (
+          <Card key={i} post={post} />
         ))}
       </section>
     </main>
