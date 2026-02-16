9114161baf9035d85ae514f422ac2b22c159e22e
seoeongeueun <erin9901@gmail.com>
2026-02-16 13:20:05 +0900

    fix: changes category fetch endpoint

--- actions/categories.ts
+++ actions/categories.ts
@@ -1,9 +1,11 @@
 import { WPCategory } from "@/types";
-import { apiRequest } from "@/lib";
+import { apiRequest, WP_BASE_URL } from "@/lib";
 
 // 기본 post 데이터는 카테고리 id만 반환하기 때문에, 카테고리 정보를 미리 가져와서 id로 매핑할 수 있도록 해야함
 export async function getCategories(): Promise<WPCategory[]> {
-  const categories = await apiRequest<WPCategory[]>("/categories");
+  const categories = await apiRequest<WPCategory[]>(
+    `${WP_BASE_URL}/categories`,
+  );
 
   return categories;
 }
