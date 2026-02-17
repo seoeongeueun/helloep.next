d7f1ec196f52da5ef9b44c2487693358e8e7435a
seoeongeueun <erin9901@gmail.com>
2026-02-17 12:33:52 +0900

    feat: implement category and tag data to card components

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,28 +1,79 @@
+"use client";
+
 import Card from "./Card";
 import List from "./List";
 import { SearchBar } from "@/ui";
-import { getPosts } from "@/actions/posts";
 import { WPPost } from "@/types";
 import type { ViewMode } from "@/types";
+import { postsQueries } from "@/query";
+import { useQuery } from "@tanstack/react-query";
+import clsx from "clsx";
 
 //TODO: 페이지네이션 적용 필요
-export default async function CardList({ viewMode }: { viewMode: ViewMode }) {
-  const posts = await getPosts(10, 1);
-  console.log("Fetched posts:", posts);
+export default function CardList({ viewMode }: { viewMode: ViewMode }) {
+  const { data: posts, isLoading, isError } = useQuery(postsQueries.list());
+
+  const renderSkeleton = () => {
+    const skeletonItems = Array.from({ length: 10 }).map((_, i) => (
+      <div
+        key={`skeleton-${i}`}
+        className={
+          viewMode === "list"
+            ? "py-2 bg-secondary animate-pulse text-transparent w-full border-b border-gray"
+            : "bg-secondary animate-pulse text-transparent w-full h-[50vh]"
+        }
+      >
+        <p aria-hidden="true">filler</p>
+      </div>
+    ));
+    return skeletonItems;
+  };
+
+  const renderPosts = () => {
+    if (!posts || posts.length === 0) {
+      return (
+        <p className="text-center w-full mt-10 text-gray text-m">
+          게시글이 없습니다
+        </p>
+      );
+    }
+
+    return posts.map((post: WPPost) =>
+      viewMode === "grid" ? (
+        <Card key={post.id} post={post} />
+      ) : (
+        <List key={post.id} post={post} />
+      ),
+    );
+  };
+
+  if (isError) {
+    return (
+      <main className="relative top-30 px-margin flex flex-col">
+        <SearchBar />
+        <div className="text-center mt-10 text-gray text-m">
+          목록을 불러올 수 없습니다
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="relative top-30 px-margin flex flex-col">
       <SearchBar />
       <section
-        className={`flex flex-wrap ${viewMode === "grid" ? "gap-x-spacing-10 py-[5rem]" : ""} w-full`}
-      >
-        {posts.map((post: WPPost, i) =>
-          viewMode === "grid" ? (
-            <Card key={i} post={post} />
-          ) : (
-            <List key={i} post={post} />
-          ),
+        className={clsx(
+          "flex flex-wrap w-full",
+          {
+            "gap-x-spacing-10 py-[5rem]": viewMode === "grid",
+          },
+          {
+            "gap-y-spacing-10 grid-cols-1 phone_large:grid-cols-2 tablet:grid-cols-4 grid":
+              isLoading,
+          },
         )}
+      >
+        {isLoading ? renderSkeleton() : renderPosts()}
       </section>
     </main>
   );
--- components/List.tsx
+++ components/List.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import { WPPost } from "@/types";
 import { stripHtmlTags } from "@/lib";
+import { Tag } from "@/ui";
+import { categoriesQueries } from "@/query";
+import { useQuery } from "@tanstack/react-query";
+import { tagsQueries } from "@/query";
+import { useMemo } from "react";
 
 interface CardProps {
   post: WPPost;
@@ -7,11 +14,38 @@ interface CardProps {
 
 //TODO: 카테고리 ID를 실제 카테고리 이름과 매핑하는 로직 필요
 export default function List({ post }: CardProps) {
+  const { data: categories } = useQuery(categoriesQueries.all());
+  const { data: tags } = useQuery(tagsQueries.all());
+
+  // post의 카테고리 id로 카테고리 데이터를 찾아서 카테고리 배열을 재생성
+  const categoryInfos = useMemo(() => {
+    if (!categories || !post.categories) return [];
+
+    return post.categories.map((catId) =>
+      categories.find((cat) => cat.id === catId),
+    );
+  }, [categories, post.categories]);
+
+  //혼용 주의 (TODO: 명칭 수정 고려) - tag는 연도 정보를 가지고 있다
+  const tagName = useMemo(() => {
+    if (!tags || !post.tags?.length) return "";
+
+    //tag는 연도 정보로 사용하고 있기 때문에 배열이어도 첫 번째 값만 사용하고 그 외는 오류로 간주
+    return tags.find((t) => t.id === post.tags[0])?.name ?? "";
+  }, [tags, post.tags]);
+
   return (
     <article className="w-full flex flex-row items-start justify-between gap-spacing-10 hover:bg-secondary border-b border-gray transtion-colors duration-200 cursor-pointer">
-      <h3 className="w-1/2 py-spacing-6 border-none!">{post.title.rendered}</h3>
+      <div className="flex flex-row w-1/2 items-center h-full gap-spacing-10">
+        <h3 className="border-none!">{post.title.rendered}</h3>
+        <div className="flex gap-spacing-3">
+          {categoryInfos.map((cat, i) => (
+            <Tag key={i} color={cat?.color || ""} label={cat?.name || ""} />
+          ))}
+        </div>
+      </div>
       <div className="w-1/2 flex flex-row gap-spacing-10 *:w-full *:py-spacing-6">
-        <time>{post.year}</time>
+        <time>{tagName}</time>
         <p className="break-keep">{stripHtmlTags(post.excerpt.rendered)}</p>
       </div>
     </article>
