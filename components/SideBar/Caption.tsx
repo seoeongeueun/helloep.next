147b796c345863481c219be489c268ece4880787
seoeongeueun <erin9901@gmail.com>
2026-02-18 12:43:48 +0900

    refactor: enhance size management and rendering logic in Caption component

--- components/SideBar/Caption.tsx
+++ components/SideBar/Caption.tsx
@@ -1,18 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 import Image from "next/image";
 import { postsQueries } from "@/query";
 import { useAppState } from "@/context/AppStateContext";
 import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
 
 export default function Caption() {
-  const [isMinimized, setIsMinimized] = useState(true);
+  const [size, setSize] = useState<0 | 1 | 2>(0); // 0: minimized, 1: expanded, 2: full screen
   const { selectedProjectId, language } = useAppState();
 
   //쿼리 내부에서 enabled로 id가 없으면 쿼리를 실행하지 않아서 그냥 넘겨도 괜찮음
   const { data: post } = useQuery(postsQueries.detail(selectedProjectId));
+
+  const pathname = usePathname();
+  const isDetailPage = /^\/\d+$/.test(pathname ?? "");
+
+  useEffect(() => {
+    if (isDetailPage) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setSize(2);
+    } else if (size === 2) {
+      setSize(0); // 디테일 페이지에서 벗어나면 기본으로
+    }
+  }, [pathname, isDetailPage, size]);
 
   if (!post || !post.content_ko) return null;
 
@@ -25,13 +38,21 @@ export default function Caption() {
     <section
       className={twMerge(
         clsx(
-          "absolute bottom-0 z-20 transition-[max-height] flex flex-row items-start py-margin text-s text-white bg-black overflow-hidden after:content-[''] after:absolute after:inset-0 after:transition-colors",
-          isMinimized
-            ? "max-h-[5rem] duration-200 cursor-pointer after:block after:w-full after:h-full after:bg-black-fade hover:after:bg-black/50"
-            : "max-h-full min-h-1/2 duration-100",
+          "absolute bottom-0 z-20 transition-[max-height] duration-1000 flex flex-row items-start py-margin text-s text-white bg-black overflow-hidden",
+          "after:content-[''] after:absolute after:inset-0 after:transition-colors",
+          {
+            "max-h-[5rem] duration-200 cursor-pointer after:block after:w-full after:h-full after:bg-black-fade hover:after:bg-black/50":
+              size === 0,
+          },
+          {
+            "max-h-96 min-h-1/2 duration-100": size === 1,
+          },
+          {
+            "max-h-full h-full duration-100": size === 2,
+          },
         ),
       )}
-      onClick={isMinimized ? () => setIsMinimized(false) : undefined}
+      onClick={size === 0 ? () => setSize(1) : undefined}
     >
       <article className="w-full word-keep space-y-spacing-10">
         {content.map((paragraph, i) => (
@@ -40,23 +61,25 @@ export default function Caption() {
           </p>
         ))}
       </article>
-      <button
-        type="button"
-        aria-label="더보기"
-        className="relative z-30 w-[4rem] h-[4rem] flex items-center justify-center cursor-pointer"
-        onClick={!isMinimized ? () => setIsMinimized(true) : undefined}
-      >
-        <Image
-          src="/icons/icon_scroll_top_arrow.svg"
-          alt="더보기"
-          width={40}
-          height={40}
-          className={clsx(
-            "transition-transform duration-200 w-fit aspect-square pointer-events-none",
-            isMinimized ? "rotate-0" : "rotate-180",
-          )}
-        />
-      </button>
+      {size !== 2 && (
+        <button
+          type="button"
+          aria-label="더보기"
+          className="relative z-30 w-[4rem] h-[4rem] flex items-center justify-center cursor-pointer"
+          onClick={size === 1 ? () => setSize(0) : undefined}
+        >
+          <Image
+            src="/icons/icon_scroll_top_arrow.svg"
+            alt="더보기"
+            width={40}
+            height={40}
+            className={clsx(
+              "transition-transform duration-200 w-fit aspect-square pointer-events-none",
+              size === 0 ? "rotate-0" : "rotate-180",
+            )}
+          />
+        </button>
+      )}
     </section>
   );
 }
