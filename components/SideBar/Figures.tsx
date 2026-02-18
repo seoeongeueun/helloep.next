15266366ee6fb395e2f69df2dec8c48530eff1c5
seoeongeueun <erin9901@gmail.com>
2026-02-18 13:13:17 +0900

    feat: enhance Figures component with initialData support and responsive styles

--- components/SideBar/Figures.tsx
+++ components/SideBar/Figures.tsx
@@ -2,18 +2,29 @@
 import { useAppState } from "@/context/AppStateContext";
 import { useQuery } from "@tanstack/react-query";
 import { postsQueries, tagsQueries, categoriesQueries } from "@/query";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { Tag } from "@/ui";
 import Image from "next/image";
+import type { WPPost, ParsedContent } from "@/types";
+import clsx from "clsx";
 
-export default function Figures() {
-  const { selectedProjectId, language } = useAppState();
+//optional initialData
+interface FiguresProps {
+  initialData?: WPPost & ParsedContent;
+}
+
+export default function Figures({ initialData }: FiguresProps) {
+  const { selectedProjectId, language, selectProject } = useAppState();
   const { data: post } = useQuery({
     ...postsQueries.detail(selectedProjectId ?? 0),
     enabled: !!selectedProjectId,
+    initialData,
   });
   const { data: tags } = useQuery(tagsQueries.all());
   const { data: categories } = useQuery(categoriesQueries.all());
+
+  //초기 데이터가 있는 경우 상세페이지기 때문에 조금 다른 css를 부여한다
+  const isDetailPage = !!initialData;
 
   const tagName = useMemo(() => {
     if (!tags || !post?.tags?.length) return "";
@@ -29,14 +40,20 @@ export default function Figures() {
   }, [categories, post]);
 
   return (
-    <section className="flex flex-col w-full h-full overflow-y-auto">
+    <section
+      className={clsx("flex flex-col w-full h-full overflow-y-auto", {
+        "p-margin": isDetailPage,
+      })}
+    >
       <header className="flex flex-row items-start justify-between gap-spacing-10 py-margin pt-0">
         <h2 className="text-[2.5rem] border-none! w-full">
           {language === "ko"
             ? post?.title_ko
             : post?.title_en || post?.title.rendered}
         </h2>
-        <time className="h-[2.3rem]">{tagName}</time>
+        <time className={clsx("h-[2.3rem]", { "ml-auto": isDetailPage })}>
+          {tagName}
+        </time>
         <ul className="flex flex-col gap-spacing-3 w-fit">
           {categoryInfos.map((cat, i) => (
             <Tag key={i} color={cat?.color || ""} label={cat?.name || ""} />
@@ -52,7 +69,7 @@ export default function Figures() {
               className="w-full h-auto"
               width={2000}
               height={1000}
-              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 80vw, 1200px"
+              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 80vw, 2000px"
             />
           </figure>
         ))}
